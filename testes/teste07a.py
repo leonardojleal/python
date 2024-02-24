bedf35215b304e1ed49dde3afdda9d29361d75ab
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-23 22:20:37 -0300

    atualização

---
+++ testes/teste07a.py
@@ -0,0 +1,20 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+
+print('A soma é {}, \n o produto é {} e a \n divisão é {:.3f}'.format(s,m,d), end=' ')
+print('Divisão inteira {} e potência {}'.format(di,e))
+
+
+#para quebrar linha = \n 
+#para não quebrar linha = end=''
+
+
+
+
+
+
