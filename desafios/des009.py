e1be426e4e38a219cc77cbd55247c3ae54b13406
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-25 16:14:19 -0300

    Tabuada

--- desafios/des009.py
+++ desafios/des009.py
@@ -1,23 +1,24 @@
 num = int(input('Digite um número: '))
 
-vx1 =num*1
-vx2 =num*2
-vx3 =num*3
-vx4 =num*4
-vx5 =num*5
-vx6 =num*6
-vx7 =num*7
-vx8 =num*8
-vx9 =num*9
-vx10 =num*10
-
-print('O valor entre {} x 1 = {}'.format(num,vx1))
-print('O valor entre {} x 2 = {}'.format(num,vx2))
-print('O valor entre {} x 3 = {}'.format(num,vx3))
-print('O valor entre {} x 4 = {}'.format(num,vx4))
-print('O valor entre {} x 5 = {}'.format(num,vx5))
-print('O valor entre {} x 6 = {}'.format(num,vx6))
-print('O valor entre {} x 7 = {}'.format(num,vx7))
-print('O valor entre {} x 8 = {}'.format(num,vx8))
-print('O valor entre {} x 9 = {}'.format(num,vx9))
-print('O valor entre {} x 10 = {}'.format(num,vx10))
+#vx1 =num*1
+#vx2 =num*2
+#vx3 =num*3
+#vx4 =num*4
+#vx5 =num*5
+#vx6 =num*6
+#vx7 =num*7
+#vx8 =num*8
+#vx9 =num*9
+#vx10 =num*10
+print('-' *12)
+print('{} x {:2} = {}'.format(num,1,num*1))
+print('{} x {:2} = {}'.format(num,2,num*2))
+print('{} x {:2} = {}'.format(num,3,num*3))
+print('{} x {:2} = {}'.format(num,4,num*4))
+print('{} x {:2} = {}'.format(num,5,num*5))
+print('{} x {:2} = {}'.format(num,6,num*6))
+print('{} x {:2} = {}'.format(num,7,num*7))
+print('{} x {:2} = {}'.format(num,8,num*8))
+print('{} x {:2} = {}'.format(num,9,num*9))
+print('{} x {:2} = {}'.format(num,10,num*10))
+print('-'*12)
