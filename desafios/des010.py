26f85db2d461c16091b27485d97f4ab27a33d3e7
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-25 16:30:20 -0300

    adicionando cotações

--- desafios/des010.py
+++ desafios/des010.py
@@ -1,5 +1,9 @@
-carteira = float(input('Quanto você tem na carteira: '))
+carteira = float(input('Quanto você tem na carteira: R$ '))
 
-dolar =carteira/3.27
+dolar =carteira/5.00
+euro =carteira/5.41
+iene =carteira/0.033
 
-print('Com a conversão do Real para Dolar, você ficaria com {:.2f} Dolares!'.format(dolar))
+print('Com a conversão atual, 25/02/2024, você ficaria com {:.2f} Dolares'.format(dolar))
+print('Com a conversão atual, 25/02/2024, você ficaria com {:.2f} Euros'.format(euro))
+print('Com a conversão atual, 25/02/2024, você ficaria com {:.2f} Ienes'.format(iene))
