5d2420b24a2e28000ddacd26a01711fc2a20760c
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-25 16:39:19 -0300

    atualização

--- desafios/des011.py
+++ desafios/des011.py
@@ -2,7 +2,7 @@ largura =float(input('Digite a largura da parede: '))
 altura =float(input('Digite a altura da parede: '))
 
 area =largura*altura
-pintar =area/2**(1/2)
+pintar =area/2
 
-print('Aqui está a área da sua parede: {} metros quadrados'.format(area))
+print('Sua parede tem a dimensão de {}X{} e sua área é de {}m2.'.format(largura,altura,area))
 print('Para pintar a parede, você vai precisar de {:.2f} litros de tinta'.format(pintar))
