8764933a5fc35897b7d761a35abb415a81f1ff06
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-25 21:27:41 -0300

    atualização

---
+++ desafios/des016.py
@@ -0,0 +1,6 @@
+import math
+num = float(input('Digite um número: '))
+int = math.floor(num)
+
+
+print('O número {} tem a parte inteira {}'.format(num,int))
---
+++ testes/teste08aa.py
@@ -0,0 +1,3 @@
+import emoji
+
+print(emoji.emojize('Olá Mundo :victory_hand:'))
