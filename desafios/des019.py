7ea4b1ed8b3f95fbe4d68a9b0186dd496569f117
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-02-26 18:28:42 -0300

    atualização

--- desafios/des019.py
+++ desafios/des019.py
@@ -4,7 +4,7 @@ a2 = input('Digite o nome do aluno 2: ')
 a3 = input('Digite o nome do aluno 3: ')
 a4 = input('Digite o nome do aluno 4: ')
 
-lista = (a1, a2, a3, a4)
+lista = [a1, a2, a3, a4]
 
 sorteio = random.choice(lista)
 print(' Dentre os alunos {}, {}, {} e {}. O aluno sorteado foi: {}'.format(a1,a2,a3,a4,sorteio))
