5e759aa03bafff9a158b4f651c63bc18bb052d23
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-02 19:52:33 -0300

    desafios

---
+++ desafios/des038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+
+if n1 > n2:
+    print('O primeiro valor, é maior que o segundo valor.')
+elif n2 > n1:
+    print('O segundo valor, é maior que o primeiro valor.')
+else:
+    print('Os valores são iguais.')        
---
+++ desafios/des039.py
@@ -0,0 +1,24 @@
+from datetime import date #biblioteca para utilizar data atual do computador
+ano = int(input('Digite seu ano de nascimento: '))
+
+#calculando 
+
+idade = date.today().year - ano #calculando o ano atual do computador, menos o ano digitado pelo usuário.
+faltam = 18 - idade
+passou = idade - 18
+
+#print('Você nasceu em {} e tem {} anos de idade'.format(ano,idade)) - TESTE FUNCIONALIDADE
+
+
+#criando lógica
+
+if idade < 18:
+    print('Você tem {} anos e ainda não precisa se alistar ao serviço militar!'.format(idade))
+    print('Faltam {} ano(s) para o seu alistamento.'.format(faltam))
+elif idade == 18:
+    print('Você tem {} anos, precisa de alistar ao serviço militar!'.format(idade))    
+else:
+    print('Você tem {} anos, passou do tempo do alistamento militar!'.format(idade))  
+    print('O seu alistamento passou {} ano(s) do prazo.'.format(passou))  
+
+print('-'*20, '\033[43m''Desenvolvido por Leonardo Leal''\033[m', '-'*20)    
