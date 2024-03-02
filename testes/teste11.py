a7d2bb3a9ca20435d4ba88c80b73707833421208
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-02 16:09:10 -0300

    criando variável cores

--- testes/teste11.py
+++ testes/teste11.py
@@ -1,5 +1,19 @@
 #print('\033[7;37mOlá, Mundo!\033[m')
 
-a = 3
+'''a = 3
 b = 5
-print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a,b))
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a,b))'''
+
+nome = 'Leonardo Leal'
+cores = {
+    'limpa': '\033[m',
+    'azul': '\033[34m',
+    'amarelo': '\033[33m',
+    'pretoebranco': '\033[7;37m'
+}
+
+
+
+
+
+print('Olá, muito prazer em te conhecer {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
