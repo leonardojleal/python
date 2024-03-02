88b68fa5d931fb159e7c9d75fd828027492e51bf
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-02 17:38:20 -0300

    cores

--- desafios/des003.py
+++ desafios/des003.py
@@ -1,5 +1,11 @@
 n1 =int(input('Digite um valor: '))
 n2 =int(input('Digite outro valor: '))
 s =n1+n2
+cores = {
+    'limpa': '\033[m',
+    'amarelo': '\033[7;40m',
+    'verde': '\033[42m'
 
-print('A soma entre os números {} e {} vale {}'.format(n1,n2,s))
+}
+
+print('A soma entre os números {}{}{} e {}{}{} vale {}{}{}'.format(cores['amarelo'],n1,cores['limpa'],cores['amarelo'],n2,cores['limpa'],cores['verde'],s,cores['limpa']))
--- desafios/des005.py
+++ desafios/des005.py
@@ -5,4 +5,4 @@ num = int(input('Digite um número: '))
 #print('O sucessor de {} é: {}'.format(num,sucessor))
 #print('O antecessor de {} é: {}'.format(num,antecessor))
 
-print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}!'.format(num, num-1,num+1))
+print('Analisando o valor \033[7;37m{}\033[m, seu antecessor é {} e o seu sucessor é {}!'.format(num, num-1,num+1))
