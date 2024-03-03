1037f6e6fc15dffdd4ff915358735e380e6e353f
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-03 13:00:30 -0300

    atualizando desafio 44

--- desafios/des044.py
+++ desafios/des044.py
@@ -1,10 +1,44 @@
-produto = float(input('Digite o valor do produto: R$ '))
+from time import sleep
 
-vista = produto - (produto*10/100) # Á vista no dinheiro / cheque
-vistac = produto - (produto*5/100) # Á vista no cartão
-x3 = produto + (produto*20/100) # 3 vezes ou mais no cartão
+print('{:=^40}'.format('LOJAS LEONARDO LEAL'))
 
-print('Para pagamentos á vista no dinheiro ou cheque, o produto terá 10% de desconto e seu novo valor será de: R$ {:.2f}'.format(vista))
-print('Para pagamentos á vista no cartão, o produto terá 5% de desconto e seu novo valor será de: R$ {:.2f}'.format(vistac))
-print('Para pagamentos em até 2x no cartão, valor normal de R$ {:.2f}'.format(produto))
-print('Para 3x ou mais no cartão, o produto terá 20% de juros e seu novo valor será de: R$ {:.2f}'.format(x3))
+preco = float(input('Preço das compras: R$ '))
+print(''' FORMAS DE PAGAMENTO
+[1] á vista dinheiro/cheque
+[2] á vista cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão
+[5] cancelamento da compra''')      
+
+opcao = int(input('Qual é a opção: '))
+
+if opcao == 1:
+    print('PROCESSANDO...')
+    sleep(2)
+    total = preco - (preco*10/100)
+elif opcao == 2:
+    print('PROCESSANDO...')
+    sleep(2)
+    total = preco - (preco*5/100) 
+elif opcao == 3:
+    print('PROCESSANDO...')
+    sleep(2)
+    total =  preco 
+    parcela = preco / 2
+    print('Sua compra será parcelada em 2x de R${:.2f}'.format(parcela))
+elif opcao == 4:
+    print('PROCESSANDO...')
+    sleep(2)
+    total = preco + (preco*20/100)
+    total_parcelas = int(input('Quantas parcelas? '))
+    parcela = total / total_parcelas
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(total_parcelas,parcela))
+else:
+    total = 0
+    print('\033[37;41mOPÇÃO INVÁLIDA DE PAGAMENTO. TENTE NOVAMENTE.\033[m')    
+
+
+
+
+print('Sua compra de R$ {:.2f} vai custar R$ \033[42m{:.2f}\033[m no final.'.format(preco,total))       
+    
