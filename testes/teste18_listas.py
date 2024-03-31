0754f0bd2bdaac66a0b11223325241917f9c3544
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-31 15:26:34 -0300

    Update teste18_listas.py

---
+++ testes/teste18_listas.py
@@ -0,0 +1,12 @@
+'''teste = list()
+teste.append('Leonardo')
+teste.append(31)
+galera = list()
+galera.append(teste[:])
+teste[0] = "Maria"
+teste[1] = 22
+galera.append(teste[:])
+print(galera)'''
+
+galera = [['joão', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+print(galera[0][0])
