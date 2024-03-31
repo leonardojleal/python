5fc400fabd95a31b001670e0b75daa1f06db7246
Leonardo Leal <158084272+leonardojleal@users.noreply.github.com>
2024-03-31 15:51:56 -0300

    Update teste18_listas.py

--- testes/teste18_listas.py
+++ testes/teste18_listas.py
@@ -8,5 +8,24 @@ teste[1] = 22
 galera.append(teste[:])
 print(galera)'''
 
-galera = [['joão', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
-print(galera[0][0])
+'''galera = [['joão', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')'''
+
+galera = list()
+dado = list()
+totmaior = totmenor = 0
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:]) #faz uma cópia
+    dado.clear()
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade')
+        totmaior += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmenor += 1
+print(f'Temos {totmaior} maiores e {totmenor} menores de idade.')        
